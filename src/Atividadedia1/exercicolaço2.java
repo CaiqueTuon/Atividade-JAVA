cbace3a54519d7ac753046f5539abfc40de7f26d
Caique Tuon <tuoncaique.gmail.com>
2021-02-25 15:37:28 -0300

    correção exercicio 2

--- src/Atividadedia1/exercicolaço2.java
+++ src/Atividadedia1/exercicolaço2.java
@@ -16,23 +16,30 @@ public class exercicolaço2 {
 			System.out.println("Entre com um numero inteiro");
 			c = ler.nextInt();
 			
-			if(a>b && b>c)
+			if(a<=b && b>=c)
 			{
 				System.out.println("A ordem : " + a + b + c);
 			}
-			if(b>a && a>c)
+			else if(b<=a && a>=c)
 			{
 				System.out.println("A ordem : " + b + a + c);
 			}
-			if(c>a && a>b)
+			else if(c<=a && a>=b)
 			{
 				System.out.println("A ordem : " + c + b + a);
 			}
-			if(c>a && a>b)
+			else if(b<=c && c>=b)
 			{
 				System.out.println("A ordem : " + b + c + a);
 			}
-
+			else if(a<=c && c>=b)
+			{
+				System.out.println("A ordem : " + a + c + b);
+			}
+			else if(c<=a && a>=b)
+			{
+				System.out.println("A ordem : " + c + a + b);
+			}
 	}
 
 }
